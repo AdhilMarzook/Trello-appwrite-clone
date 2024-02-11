56b5750cb173e7d04a89be5972743bd125b1c1f4
AdhilMarzook <adhilmarzook20@gmail.com>
2024-02-11 11:48:25 +0000

    commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Board from '@/components/Board';
+import Header from '@/components/Header';
+
+export default function Home() {
+  return (
+    <main>
+      {/*Header*/}
+      <Header/>
+      {/*board time-3.48.33*/}
+      <Board/>
+    </main>
+  );
+}
+ 
